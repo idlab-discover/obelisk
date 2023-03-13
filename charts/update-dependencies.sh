e9229b6c47285dfa041c2df686c9a46bcd6d4469
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2023-03-13 09:33:56 +0100

    ci: fix update dependencies script

--- charts/update-dependencies.sh
+++ charts/update-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 PARALLEL="$(nproc --ignore=1)"
-find . -maxdepth 1 -mindepth 1 -type d -print0 | xargs -t -0 -n1 -P "$PARALLEL" helm dependency update --skip-refresh
+find . -maxdepth 2 -name Chart.yaml  -printf "%h\0" | xargs -t -0 -n1 -P "$PARALLEL" helm dependency update --skip-refresh
